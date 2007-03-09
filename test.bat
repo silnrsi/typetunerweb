dff0e2b3e4f50e0c235d4dd92fbaea4ac9e86b08
Alan Ward <alan_ward@sil.org>
2007-03-09 00:57:54 +0000

    make more robust
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@19 4150712b-26e8-44b9-9db8-fb9803e0bbe7

--- test.bat
+++ test.bat
@@ -6,27 +6,33 @@ if "%1"=="clean" goto clean
 
 if not exist zork.ttf copy DoulosSILR.ttf zork.ttf
 
+if exist zork_tt.ttf del zork_tt.ttf
 %TT% add feat_all.xml zork.ttf
 pause
 ttfdump -tx -nx zork_tt.ttf
 pause
+if exist feat.xml del feat.xml
 %TT% createset zork_tt.ttf feat.xml
 fc feat_set_def.xml feat.xml
 pause
+if exist zork_tt_tt.ttf del zork_tt_tt.ttf
 %TT% applyset feat_set.xml zork_tt.ttf
 pause
 if exist zork_tt_tt_1.ttf del zork_tt_tt_1.ttf
 ren zork_tt_tt.ttf zork_tt_tt_1.ttf
+if exist zork_tt_tt.ttf del zork_tt_tt.ttf
 %TT% applyset feat_set_2.xml zork_tt.ttf
 pause
 if exist zork_tt_tt_2.ttf del zork_tt_tt_2.ttf
 ren zork_tt_tt.ttf zork_tt_tt_2.ttf
 ttfdump -tx -nx zork_tt_tt_2.ttf
 pause
+if exist feat.xml del feat.xml
 %TT% extract zork_tt_tt_2.ttf feat.xml
 fc feat_set_2.xml feat.xml
 pause
 copy zork_tt_tt_2.ttf foo.ttf
+if exist foo_tt.ttf del foo_tt.ttf
 %TT% delete foo.ttf
 pause
 ttfdump -tx -nx foo_tt.ttf
