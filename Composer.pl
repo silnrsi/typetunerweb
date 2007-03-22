e4124123c26e0982baed7d9062f55d08ac59863a
Alan Ward <alan_ward@sil.org>
2007-03-22 23:41:04 +0000

    make Tag_get() return nicer tags for settings parse GSI XML file write sub to calculate feature interactions for chars affected by more than one feature store ids as strings in %feats rename two variables
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@31 4150712b-26e8-44b9-9db8-fb9803e0bbe7

--- Composer.pl
+++ Composer.pl
@@ -1,5 +1,7 @@
 # Copyright (c) SIL International, 2007. All rights reserved.
 
+#Script to create a template for the TypeTuner feat_all.xml file for our Roman fonts.
+
 #TODO: handle variant glyphs without $feat but with var_uid & ps_name stored too?
 #TODO: handle double encoded glyphs (deprecated PUA)
 
@@ -19,30 +21,42 @@ my $feat_all_elem = "all_features";
 
 #### subroutines ####
 
-my (%tags); #used only by below sub
+my (%tags, $prev_feat_tag); #used only by below sub
 sub Tag_get($$)
 #get a tag for a given string
+#second arg indicates a feature (2) or a setting (1)
+#assumes feature tags are created before setting tags
 #TODO: implement this using a table lookup (probably using <DATA>)
 #       to get more sensible tags. probably won't need second arg
 {
 	my ($str, $cnt) = @_;
 	my ($tmp);
 
-	if ($str eq "True") {return 't';}
-	if ($str eq "False") {return 'f';}
+	#if ($str eq "True") {return 't';}
+	#if ($str eq "False") {return 'f';}
 	
-	$tmp = substr($str, 0, $cnt);
+	$tmp = $str;
+	$tmp =~ s/\s//;
+	$tmp = substr($tmp, 0, $cnt);
+	
 	if ($cnt == 1)
-		{$tmp = lc($tmp);}
+	{#setting tags
+		$tmp = lc($tmp);
+		while (defined($tags{$prev_feat_tag}{'settings'}{$tmp}))
+			{substr($tmp, -1, 1, chr(ord(substr($tmp, -1, 1)) + 1));} #changes A to B
+		$tags{$prev_feat_tag}{'settings'}{$tmp} = $str;
+	}
 	elsif ($cnt == 2)
-		{$tmp = uc($tmp);}
+	{#feature tags
+		$tmp = uc($tmp);
+		while (defined($tags{$tmp}))
+			{substr($tmp, -1, 1, chr(ord(substr($tmp, -1, 1)) + 1));} #changes AA to AB
+		$tags{$tmp} = {'str' => $str}; #might as well store something useful
+		$prev_feat_tag = $tmp;
+	}
 	else {die("invalid second arg to Tag_get: $cnt\n");}
 	
-	while (defined($tags{$tmp}))
-		{substr($tmp, -1, 1, chr(ord(substr($tmp, -1, 1)) + 1));}
-	$tags{$tmp} = $str;
-	
-	return "$tmp";
+	return $tmp;
 }
 
 sub Feats_get($\%)
@@ -61,8 +75,8 @@ sub Feats_get($\%)
 		foreach $set_id (sort keys %{$feat->{'settings'}})
 		{
 			($feat_nm, $set_nm) = $Feat_tbl->settingName($feat_id, $set_id);
-			#if ($opt_d) {print "Feats_get: feat_nm: $feat_nm set_nm: $set_nm\n";}
 			
+			($feat_id, $set_id) = ("$feat_id", "$set_id");
 			if (not defined $feats->{$feat_id})
 			{# this could go in the outer loop
 			 #  but it is nice after the settingName call
@@ -71,18 +85,16 @@ sub Feats_get($\%)
 				$feats->{$feat_id}{'tag'} = $feat_tag;
 				$feats->{$feat_id}{'default'} = $set_id;
 				if (not defined($feats->{' ids'}))
-					{$feats->{' ids'} = [$feat_id];}
-				else
-					{push(@{$feats->{' ids'}}, $feat_id);}
+					{$feats->{' ids'} = [];}
+				push(@{$feats->{' ids'}}, $feat_id);
 			}
 			
 			$set_tag = Tag_get($set_nm, 1);
 			$feats->{$feat_id}{'settings'}{$set_id}{'name'} = $set_nm;
 			$feats->{$feat_id}{'settings'}{$set_id}{'tag'} = $set_tag;
 			if (not defined($feats->{$feat_id}{'settings'}{' ids'}))
-				{$feats->{$feat_id}{'settings'}{' ids'} = [$set_id];}
-			else
-				{push(@{$feats->{$feat_id}{'settings'}{' ids'}}, $set_id);}
+				{$feats->{$feat_id}{'settings'}{' ids'} = [];}
+			push(@{$feats->{$feat_id}{'settings'}{' ids'}}, $set_id);
 		}
 	}
 	
@@ -106,55 +118,156 @@ sub Feats_get($\%)
 	}
 }
 
-sub Gsi_xml_parse($\%\%)
+sub Featset_combos_get($@)
+#return an array of strings where each string indicates feature interactions
+# handles multi-valued features which don't interact
+# TODO: properly handle a binary-valued feature interacting with a multi-valued feature
+#        currently combos are only calculated using the first multi-value found
+{
+	my ($feat_add, @feats) = @_;
+	my (@feats_combo);
+	
+	#only allow one multi-valued featset to interact
+	foreach (@feats)
+		{if (substr($feat_add, 0, 2) eq substr($_, 0, 2))
+			{return @feats_combo;}} #@feats_combo is empty
+	
+#	see todo above
+#	my ($multi_found, $bin_found) = (0, 0);
+#	foreach (@feats, $feat_add)
+#	{#this does not work, need better test to determine if a multi or binary value
+#		my $set = substr($_, -1, 1);
+#		if ($set ne 't' && $set ne 'f')
+#			{$multi_found = 1;}
+#		else
+#			{$bin_found = 1;}
+#	}
+#	if ($multi_found && $bin_found)
+#		{die "error: multi-valued an binary-valued features interact: $feat_add, @feats\n";}
+#	if ($multi_found)
+#		{@feats_combo = (undef);return @feats_combo;}
+
+	if (scalar @feats == 1)
+	{
+		push(@feats_combo, join(' ', sort($feats[0], $feat_add)));
+	}
+	elsif (scalar @feats == 2)
+	{
+		push (@feats_combo, join(' ', sort($feats[0], $feat_add)));
+		push (@feats_combo, join(' ', sort($feats[1], $feat_add)));
+		#push (@feats_combo, join(' ', sort($feats[0], $feats[1]))); should already be handled
+		push (@feats_combo, join(' ', sort($feats[0], $feats[1], $feat_add)));
+	}
+	else
+	{
+		die("too many features interacting: $feat_add, @feats\n");
+	}
+	
+	return @feats_combo;
+}
+
+sub Gsi_xml_parse($\%\%\%)
 #parse the GSI xml file to create the structures describing
 # mapping to PS name for given USV and feature setting and
 # mapping from feature settings to list of USVs affected
 {
-	my ($gsi_fn, $ps_name_for_usv_feat, $usvs_for_featset) = @_;
-	my ($xml_parser);
+	my ($gsi_fn, $feats, $usv_feat_to_ps_name, $featset_to_usvs) = @_;
+	my ($xml_parser, $ps_name, $var_uid_capture, $var_uid, $feat_found);
 
 	$xml_parser = XML::Parser::Expat->new();
 	$xml_parser->setHandlers('Start' => sub {
 		my ($xml_parser, $tag, %attrs) = @_;
-		if ($tag eq '')
+		if ($tag eq 'ps_name')
 		{
+			$ps_name = $attrs{'value'};
+			$feat_found = 0;
 		}
-		elsif ($tag eq '')
+		elsif ($tag eq 'var_uid')
 		{
+			$var_uid = undef;
+			$var_uid_capture = 1;
+		}
+		elsif ($tag eq 'feature')
+		{
+			if (not defined($var_uid)) {die("not USV for feature: $attrs{'category'}")};
+			my $usv = substr($var_uid, 2);
+			
+			my $feat = $attrs{'category'};
+			my $set;
+			if (defined ($attrs{'value'}))
+				{$set = $attrs{'value'};}
+			else
+				{$set = '1';}
+			my $featset = $feats->{$feat}{'tag'} . $feats->{$feat}{'settings'}{$set}{'tag'};
+			
+			if (not defined ($featset_to_usvs->{$featset}))
+				{$featset_to_usvs->{$featset} = [];}
+			push(@{$featset_to_usvs->{$featset}}, $usv);
+			
+			#handle interacting features
+			my @prev_feats;
+			foreach (keys %{$usv_feat_to_ps_name->{$usv}})
+				{if ($_ ne 'unk') {push(@prev_feats, $_);}} #nothing interacts with unk features
+			if (scalar @prev_feats)
+			{
+				my @featset_combos = Featset_combos_get($featset, @prev_feats);
+				foreach $featset (@featset_combos)
+				{
+					if (not defined($featset_to_usvs->{$featset}))
+						{$featset_to_usvs->{$featset} = [];}
+					push(@{$featset_to_usvs->{$featset}}, $usv);
+				}
+			}
+			
+			$usv_feat_to_ps_name->{$usv}{$featset} = $ps_name;
+			$feat_found = 1;
 		}
 		else
-		{
-		}
+		{}
 	}, 'End' => sub {
 		my ($xml_parser, $tag) = @_;
-		if ($tag eq '')
+		if ($tag eq 'ps_name')
 		{
+			if (!$feat_found && defined($var_uid))
+			{ #use 'unk' featset to store info on variant glyphs w/o features
+				my $usv = substr($var_uid, 2);
+				if (not defined($usv_feat_to_ps_name->{$usv}{'unk'}))
+					{$usv_feat_to_ps_name->{$usv}{'unk'} = []}
+				push(@{$usv_feat_to_ps_name->{$usv}{'unk'}}, $ps_name);
+			} 
+			$ps_name = undef;
 		}
-		elsif ($tag eq '')
+		elsif ($tag eq 'var_uid')
 		{
+			$var_uid_capture = 0;
 		}
 		else
-		{
-		}
+		{}
 	}, 'Char' => sub {
 		my ($xml_parser, $str) = @_;
-		#die ("XML element content not allowed: $str\n");
+		if ($var_uid_capture)
+			{$var_uid .= $str;}
 	});
 
 	$xml_parser->parsefile($gsi_fn) or die "Can't read $gsi_fn";
+	
+	if ($opt_d)
+	{
+		foreach (sort keys %$usv_feat_to_ps_name) {print "$_ "}; print "\n";
+		foreach (sort keys %$featset_to_usvs) {print "($_)"}; print "\n";
+	}
 }
 
 sub Features_output($\%\%\%)
 #output the <feature>s elements
 {
-	my ($feat_all_fh, $feats, $usvs_for_featset, $ps_name_for_usv_feat) = @_;
+	my ($feat_all_fh, $feats, $featset_to_usvs, $usv_feat_to_ps_name) = @_;
 }
 
 sub Interactions_output($\%\%)
 #output the <interactions> elements
 {
-	my ($feat_all_fh, $ps_name_for_usv_feat, $usvs_for_featset) = @_;
+	my ($feat_all_fh, $usv_feat_to_ps_name, $featset_to_usvs) = @_;
 	
 	print $feat_all_fh "\t<interactions>\n";
 
@@ -198,7 +311,7 @@ END
 sub cmd_line_exec() #for UltraEdit function list
 {}
 
-my (%feats, %ps_name_for_usv_feat, %usvs_for_featset, $feat_all_fh);
+my (%feats, %usv_feat_to_ps_name, %featset_to_usvs, $feat_all_fh);
 my ($font_fn, $gsi_fn, $feat_all_fn);
 
 getopts('d'); #sets $opt_d & removes the switch from @ARGV
@@ -209,7 +322,7 @@ if (scalar @ARGV != 2)
 ($font_fn, $gsi_fn) = ($ARGV[0], $ARGV[1]);
 
 Feats_get($font_fn, %feats);
-Gsi_xml_parse($gsi_fn, %ps_name_for_usv_feat, %usvs_for_featset);
+Gsi_xml_parse($gsi_fn, %feats, %usv_feat_to_ps_name, %featset_to_usvs);
 
 $feat_all_fn = $feat_all_base_fn; #todo adjust based on path to $font_fn
 open $feat_all_fh, ">$feat_all_fn" or die("Could not open $feat_all_fn for writing\n");
@@ -217,8 +330,8 @@ print $feat_all_fh "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 print $feat_all_fh "<!DOCTYPE all_features SYSTEM \"feat_all.dtd\">\n";
 print $feat_all_fh "<all_features version=\"1.0\">\n";
 
-Features_output($feat_all_fh, %feats, %usvs_for_featset, %ps_name_for_usv_feat);
-Interactions_output($feat_all_fh, %ps_name_for_usv_feat, %usvs_for_featset);
+Features_output($feat_all_fh, %feats, %featset_to_usvs, %usv_feat_to_ps_name);
+Interactions_output($feat_all_fh, %usv_feat_to_ps_name, %featset_to_usvs);
 Aliases_output($feat_all_fh); 
 
 print $feat_all_fh "</all_features>\n";
