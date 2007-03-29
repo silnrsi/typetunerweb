2d33ba8dee06e8c22cb003bba9b16e6f0be60e35
Alan Ward <alan_ward@sil.org>
2007-03-29 00:33:23 +0000

    bat file to test Composer then test TypeTuner with maximal tuning
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@56 4150712b-26e8-44b9-9db8-fb9803e0bbe7

---
+++ test_tt_w_comp.bat
@@ -0,0 +1,34 @@
+rem create a feat_all file with no options to choose for testing
+rem create a feat_set file with all settings set to non-default values
+rem tune a font maximally
+
+cls
+if "%1"=="" goto usage
+if "%1"=="pl" set TT=perl typetuner.pl -d
+if "%1"=="pl" set CO=perl composer.pl -d
+if "%1"=="exe" set TT=typetuner.exe -d
+if "%1"=="exe" set CO=composer.exe -d
+if "%1"=="clean" goto clean
+
+if not exist eggs.ttf copy DoulosSILR_4100.ttf eggs.ttf
+
+%CO% -t eggs.ttf gsi_eggs.xml
+pause
+%TT% -t createset feat_all_composer.xml feat_set_composer.xml
+pause
+%TT% applyset_xml feat_all_composer.xml feat_set_composer.xml eggs.ttf
+goto end
+
+:clean
+if exist feat_all_composer.xml del feat_all_composer.xml
+if exist feat_set_composer.xml del feat_set_composer.xml
+if exist eggs_tt.ttf del eggs_tt.ttf
+goto end
+
+:usage
+@echo usage:
+@echo test_tt_w_comp pl - to test perl version
+@echo OR
+@echo test_tt_w_comp exe - to test exe version
+
+:end
