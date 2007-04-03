06e124b88796a729a1535fec8463d956f7c70964
Alan Ward <alan_ward@sil.org>
2007-04-03 19:13:22 +0000

    Update this file since Font::TTF lib includes my bug fixes
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@65 4150712b-26e8-44b9-9db8-fb9803e0bbe7

--- copy_Feat.bat
+++ copy_Feat.bat
@@ -1,4 +1,4 @@
 copy c:\perl\site\lib\font\ttf\font.pm .
 copy c:\perl\site\lib\font\ttf\grfeat.pm .
 copy c:\perl\site\lib\font\ttf\useall.pm .
-copy c:\perl\site\lib\font\ttf\ttopen.pm .
+rem copy c:\perl\site\lib\font\ttf\ttopen.pm .
