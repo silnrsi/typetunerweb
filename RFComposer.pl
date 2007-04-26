5097f38c4c01c994d0d9c7025f41b64c53d8a21b
Alan Ward <alan_ward@sil.org>
2007-04-26 22:27:23 +0000

    specify tags for features & values insure unique tags are generated make -l switch output feature & value names only once eliminate use of feature id types insure "Line spacing" feature tags are handled correctly
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@102 4150712b-26e8-44b9-9db8-fb9803e0bbe7

--- RFComposer.pl
+++ RFComposer.pl
@@ -25,127 +25,88 @@ my $feat_all_base_fn = 'feat_all_composer.xml';
 my $feat_all_elem = "all_features";
 
 #all the ids must be 4 digits
-#these lists can overlap. eg vietnamese stacking needs both variant glyphs & OT struct mods
-my $graphite_only_feats = '1026 1050 1051 1052 1062';
 my $vietnamese_style_diacs_feat = '1029';
 my $romanian_style_diacs_feat = '1041';
-my $variant_feats = '1024 1025 1027 1029 1028 1030 1031 1032 1033 1034 1035 1036';
-$variant_feats .= '1037 1038 1039 1040 1041 1042 1043 1044 1045 1046 1047 1048 1049';
-$variant_feats .= '1053 1054 1055 1056 1057 1059 1060 1061';
 
 #generated using the -l switch 
-# then copying & pasting the file produced into this file
+# then copying & pasting the file produced into this file.
+# used calls to &Tag_get to create initially then commented them out
+# and now use &Tag_lookup calls to maintain
 my %nm_to_tag = (
-	'Tone numbers' => 'TO',
-	'False' => 'f',
-	'True' => 't',
-	'Hide tone contour staves' => 'HI',
-	'False' => 'f',
-	'True' => 't',
-	'9-level pitches' => 'NI',
-	'Ligated' => 'l',
-	'Show tramlines' => 's',
-	'Non-ligated' => 'n',
-	'Show tramlines, non-ligated' => 't',
-	'Vietnamese-style diacritics' => 'VI',
-	'False' => 'f',
-	'True' => 't',
-	'Romanian-style diacritics' => 'RO',
-	'False' => 'f',
-	'True' => 't',
-	'Chinantec tones' => 'CH',
-	'False' => 'f',
-	'True' => 't',
-	'Bridging diacritics' => 'BR',
-	'False' => 'f',
-	'True' => 't',
-	'Barred-bowl forms' => 'BA',
-	'False' => 'f',
-	'True' => 't',
-	'Literacy alternates' => 'LI',
-	'False' => 'f',
-	'True' => 't',
-	'Slant italic specials' => 'SL',
-	'False' => 'f',
-	'True' => 't',
-	'Uppercase Eng alternates' => 'UP',
-	'Large eng with descender' => 'l',
-	'Large eng on baseline' => 'm',
-	'Capital N with tail' => 'c',
-	'Large eng with short stem' => 'n',
-	'Rams horn alternates' => 'RA',
-	'Small bowl' => 's',
-	'Large bowl' => 'l',
-	'Small gamma' => 't',
-	'Ogonek alternate' => 'OG',
-	'Curved' => 'c',
-	'Straight' => 's',
-	'Capital H-stroke alternate' => 'CA',
-	'Horizontal stroke' => 'h',
-	'Vertical stroke' => 'v',
-	'J stroke hook alternate' => 'JS',
-	'No serif' => 'n',
-	'Top serif' => 't',
-	'Capital N-left-hook alternate' => 'CB',
-	'Uppercase style' => 'u',
-	'Lowercase style' => 'l',
-	'Open-O alternate' => 'OP',
-	'Bottom serif' => 'b',
-	'Top serif' => 't',
-	'Small p-hook alternate' => 'SM',
-	'Left hook' => 'l',
-	'Right hook' => 'r',
-	'Capital R-tail alternate' => 'CC',
-	'Uppercase style' => 'u',
-	'Lowercase style' => 'l',
-	'Capital T-hook alternate' => 'CD',
-	'Left hook' => 'l',
-	'Right hook' => 'r',
-	'Small v-hook alternate' => 'SN',
-	'Curved' => 'c',
-	'Straight' => 's',
-	'Capital Y-hook alternate' => 'CE',
-	'Left hook' => 'l',
-	'Right hook' => 'r',
-	'Small ezh-curl alternate' => 'SO',
-	'Small bowl' => 's',
-	'Large bowl' => 'l',
-	'Capital Ezh alternates' => 'CF',
-	'Normal' => 'n',
-	'Reversed sigma' => 'r',
-	'OU alternates' => 'OU',
-	'Closed' => 'c',
-	'Open' => 'o',
-	'Cyrillic E alternates' => 'CY',
-	'False' => 'f',
-	'True' => 't',
-	'Modifier apostrophe alternates' => 'MO',
-	'Small' => 's',
-	'Large' => 'l',
-	'Modifier colon alternate' => 'MP',
-	'Tight' => 't',
-	'Wide' => 'w',
-	'Combining breve Cyrillic form' => 'CO',
-	'False' => 'f',
-	'True' => 't',
-	'Cyrillic shha alternate' => 'CZ',
-	'False' => 'f',
-	'True' => 't',
-	'Empty set alternates' => 'EM',
-	'Circle' => 'c',
-	'Zero' => 'z',
-	'Show invisible characters' => 'SH',
-	'False' => 'f',
-	'True' => 't',
-	'Diacritic selection' => 'DI',
-	'False' => 'f',
-	'True' => 't',
-	'Line spacing' => 'LJ',
+	'Tone numbers' => 'TnNmbr',
+	'False' => 'F',
+	'True' => 'T',
+	'Hide tone contour staves' => 'TnStvsHd',
+	'9-level pitches' => '9lvl',
+	'Ligated' => 'Lgt',
+	'Show tramlines' => 'TrmLn',
+	'Non-ligated' => 'NoLgt',
+	'Show tramlines, non-ligated' => 'TrmLnNoLgt',
+	'Vietnamese-style diacritics' => 'VtnmDiacs',
+	'Romanian-style diacritics' => 'RmnDiacs',
+	'Chinantec tones' => 'Chntc',
+	'Bridging diacritics' => 'BrdgDiacs',
+	'Barred-bowl forms' => 'BarBwl',
+	'Literacy alternates' => 'Lit',
+	'Slant italic specials' => 'SlntItlc',
+	'Uppercase Eng alternates' => 'Eng',
+	'Large eng with descender' => 'LgDsc',
+	'Large eng on baseline' => 'LgBsln',
+	'Capital N with tail' => 'CapN',
+	'Large eng with short stem' => 'LgShrtStm',
+	'Rams horn alternates' => 'RmHrn',
+	'Small bowl' => 'Sm',
+	'Large bowl' => 'Lrg',
+	'Small gamma' => 'Gma',
+	'Ogonek alternate' => 'Ognk',
+	'Curved' => 'Crv',
+	'Straight' => 'Strt',
+	'Capital H-stroke alternate' => 'LgHStrk',
+	'Horizontal stroke' => 'Hrz',
+	'Vertical stroke' => 'Vrt',
+	'J stroke hook alternate' => 'JStrkHk',
+	'No serif' => 'NoSrf',
+	'Top serif' => 'TopSrf',
+	'Capital N-left-hook alternate' => 'LgNLftHk',
+	'Uppercase style' => 'Uc',
+	'Lowercase style' => 'Lc',
+	'Open-O alternate' => 'OpnO',
+	'Bottom serif' => 'BtmSrf',
+	'Small p-hook alternate' => 'SmPHk',
+	'Left hook' => 'LftHk',
+	'Right hook' => 'RtHk',
+	'Capital R-tail alternate' => 'LgRTl',
+	'Capital T-hook alternate' => 'LgTHk',
+	'Small v-hook alternate' => 'SmVHk',
+	'Capital Y-hook alternate' => 'LgYHk',
+	'Small ezh-curl alternate' => 'SmEzhCrl',
+	'Capital Ezh alternates' => 'LgEzh',
+	'Normal' => 'Nrml',
+	'Reversed sigma' => 'RvSgma',
+	'OU alternates' => 'Ou',
+	'Closed' => 'Clsd',
+	'Open' => 'Opn',
+	'Cyrillic E alternates' => 'CyrE',
+	'Modifier apostrophe alternates' => 'ModAp',
+	'Small' => 'Sm',
+	'Large' => 'Lg',
+	'Modifier colon alternate' => 'ModCol',
+	'Tight' => 'Tght',
+	'Wide' => 'Wd',
+	'Combining breve Cyrillic form' => 'CmbBrvCyr',
+	'Cyrillic shha alternate' => 'CyShha',
+	'Empty set alternates' => 'EmpSet',
+	'Circle' => 'Crcl',
+	'Zero' => 'Zro',
+	'Show invisible characters' => 'ShwInv',
+	'Diacritic selection' => 'DiacSlct',
+	'Line spacing' => 'LnSpc',
+	'Loose' => 'Ls',
 );
 
 #### subroutines ####
 
-my (%tags, $prev_feat_tag); #used only by below sub
+my (%tags, $prev_feat_tag); #used only by two below sub
 sub Tag_get($$)
 #get a tag for a given string
 #second arg indicates a feature (2) or a setting (1)
@@ -187,9 +148,15 @@ sub Tag_lookup($\%)
 {
 	my ($name, $nm_to_tag) = @_;
 	if (defined $nm_to_tag->{$name})
-		{return $nm_to_tag->{$name};}
+	{
+		$tags{$nm_to_tag->{$name}} = {'str' => $name}; #insures Tag_get tags are unique
+		return $nm_to_tag->{$name};
+	}
 	else
-		{print "new name so generating tag: $name\n"; return Tag_get($name, 2);}
+	{
+		print "*** new name found so generating a tag - name:$name\n";
+		return Tag_get($name, 2); #always get two-letter tags
+	}
 }
 
 sub Feats_get($\%)
@@ -243,7 +210,7 @@ sub Feats_get($\%)
 		{
 			my $feat_t = $feats->{$feat_id};
 			my ($tag, $name, $default) = ($feat_t->{'tag'}, $feat_t->{'name'}, 
-			                              $feat_t->{'default'});
+										  $feat_t->{'default'});
 			print "feature: $feat_id tag: $tag name: $name default: $default\n";
 			foreach $set_id (@{$feats->{$feat_id}{'settings'}{' ids'}})
 			{
@@ -299,7 +266,7 @@ sub Gsi_xml_parse($\%\%\%)
 	my ($gsi_fn, $feats, $usv_feat_to_ps_name, $featset_to_usvs) = @_;
 	
 	my ($xml_parser, $active, $ps_name, $feat_found, $var_uid_capture, $var_uid);
-    	$xml_parser = XML::Parser::Expat->new();
+	$xml_parser = XML::Parser::Expat->new();
 	$xml_parser->setHandlers('Start' => sub {
 		my ($xml_parser, $tag, %attrs) = @_;
 		if ($tag eq 'glyph')
@@ -435,10 +402,10 @@ sub Special_glyphs_handle(\%\%\%)
 	my ($feats, $usv_feat_to_ps_name, $featset_to_usvs) = @_;
 	
 	#add uni01B7.RevSigmaStyle as a variant for U+01B7 for feature Capital Ezh alternates (1042)
-    # this is a variant glyph that is also encoded in the PUA (F217)
-    # so there is no <var_uid> in the GSI data for it
-    # the below code doesn't handle the case where 01B7 has more than one variant, so die if that occurs
-    if (defined $usv_feat_to_ps_name->{'01B7'}) {die "status of 01B7 has changed\n";}
+	# this is a variant glyph that is also encoded in the PUA (F217)
+	# so there is no <var_uid> in the GSI data for it
+	# the below code doesn't handle the case where 01B7 has more than one variant, so die if that occurs
+	if (defined $usv_feat_to_ps_name->{'01B7'}) {die "status of 01B7 has changed\n";}
 	my $feat_tag = $feats->{'1042'}{'tag'};
 	my $set_id = $feats->{'1042'}{'settings'}{' ids'}[1];
 	my $set_tag = $feats->{'1042'}{'settings'}{$set_id}{'tag'};
@@ -478,8 +445,8 @@ sub Features_output($\%\%\%\%)
 {
 	my ($feat_all_fh, $feats, $featset_to_usvs, $usv_feat_to_ps_name, $dblenc_usv) = @_;
 	my $fh = $feat_all_fh;
-    my ($feat_id, $set_id);
-    
+	my ($feat_id, $set_id);
+	
 	foreach $feat_id (@{$feats->{' ids'}})
 	{
 		my $feat = $feats->{$feat_id};
@@ -512,16 +479,16 @@ sub Features_output($\%\%\%\%)
 				goto cmd_end;
 			}
 			
+			my $flag = 0;
+			
 			#gr_feat cmd
 			unless ($opt_q)
-				{print $fh "\t\t\t<cmd name=\"gr_feat\" args=\"$feat_id $set_id\"/>\n";}
-				
-			my $flag = 0;
-			if ($graphite_only_feats =~ /$feat_id/ or $opt_g)
 			{
+				print $fh "\t\t\t<cmd name=\"gr_feat\" args=\"$feat_id $set_id\"/>\n";
 				$flag = 1;
 			}
 			
+			#OT cmds
 			if ($vietnamese_style_diacs_feat =~ /$feat_id/ and not $opt_g)
 			{#hard-coded
 				print $fh "\t\t\t<cmd name=\"lookup_add\" args=\"GSUB {ccmp_latin} {viet_decomp}\"/>\n";
@@ -538,11 +505,11 @@ sub Features_output($\%\%\%\%)
 				$flag = 1;
 			}
 			
-			if ($variant_feats =~ /$feat_id/ and not $opt_g)
+			#encode cmds
+			my $featset = "$feat_tag-$set_tag";
+			if (defined($featset_to_usvs->{$featset}) and not $opt_g)
 			{#write one cmd for each variant glyph associated with this feature setting
-				my $featset = "$feat_tag-$set_tag";
 				my @usvs = @{$featset_to_usvs->{$featset}};
-				if (not @usvs) {die("feature is not of variant type: $feat_id $featset\n");}
 				my ($usv, $ps_name);
 				foreach $usv (@usvs)
 				{
@@ -550,14 +517,12 @@ sub Features_output($\%\%\%\%)
 					print $fh "\t\t\t<cmd name=\"encode\" args=\"$usv $ps_name\"/>\n";
 					if (defined $dblenc_usv->{$usv})
 						{print $fh "\t\t\t<cmd name=\"encode\" args=\"$dblenc_usv->{$usv} $ps_name\"/>\n";}
-                }
-                $flag = 1;
+				}
+				$flag = 1;
 			}
 			
-			if (not $flag and not $opt_g)
+			if (not $flag)
 			{
-				print "WARNING: type of feature unknown: $feat_id\n";
-				print $fh "\t\t\t<!-- type of feature unknown -->\n";
 				print $fh "\t\t\t<cmd name name=\"null\" args=\"null\"/>\n";
 			};
 			
@@ -572,51 +537,54 @@ sub Features_output($\%\%\%\%)
 	
 	#output line spacing feature
 	unless ($opt_g)
-		{
+	{
 		my $line_gap_tag = Tag_lookup('Line spacing', %nm_to_tag);
+		my $tight_tag = Tag_lookup('Tight', %nm_to_tag);
+		my $normal_tag = Tag_lookup('Normal', %nm_to_tag);
+		my $loose_tag = Tag_lookup('Loose', %nm_to_tag);
 		if (not $opt_t)
 		{ #be careful of tabs in section below for proper output
-    		print $fh <<END
+			print $fh <<END
 	<feature name="Line spacing" value="Normal" tag="$line_gap_tag">
 		<!-- edit the below lines to provide the correct line metrics -->
 		<!-- Doulos -->
-		<value name="Normal" tag="n">
+		<value name="Normal" tag="$normal_tag">
 			<cmd name="null" args="2324 810"/>
 		</value>
-		<value name="Tight" tag="t">
+		<value name="Tight" tag="$tight_tag">
 			<cmd name="line_metrics" args="1420 442 307 1825 443 1825 443 87"/>
 		</value>
-		<value name="Loose" tag="l">
+		<value name="Loose" tag="$loose_tag">
 			<cmd name="line_gap" args="2800 1100"/>
 		</value>
 		<!-- Charis -->
-		<value name="Normal" tag="n">
+		<value name="Normal" tag="$normal_tag">
 			<cmd name="null" args="2450 900"/>
 		</value>
-		<value name="Tight" tag="t">
+		<value name="Tight" tag="$tight_tag">
 			<cmd name="line_gap" args="1950 500"/>
 		</value>
-		<value name="Loose" tag="l">
+		<value name="Loose" tag="$loose_tag">
 			<cmd name="line_gap" args="2900 1200"/>
 		</value>
 		<!-- Gentium -->
-		<value name="Normal" tag="n">
+		<value name="Normal" tag="$normal_tag">
 			<cmd name="null" args="2050 900"/>
 		</value>
-		<value name="Tight" tag="t">
+		<value name="Tight" tag="$tight_tag">
 			<cmd name="line_gap" args="1750 550"/>
 		</value>
-		<value name="Loose" tag="l">
+		<value name="Loose" tag="$loose_tag">
 			<cmd name="line_gap" args="2450 1200"/>
 		</value>
 		<!-- Andika -->
-		<value name="Normal" tag="n">
+		<value name="Normal" tag="$normal_tag">
 			<cmd name="null" args="2500 800"/>
 		</value>
-		<value name="Tight" tag="t">
+		<value name="Tight" tag="$tight_tag">
 			<cmd name="line_gap" args="2100 550"/>
 		</value>
-		<value name="Loose" tag="l">
+		<value name="Loose" tag="$loose_tag">
 			<cmd name="line_gap" args="2900 1100"/>
 		</value>
 	</feature>
@@ -624,17 +592,16 @@ END
 		}
 		else
 		{ #be careful of tabs in section below for proper output
-   			print $fh <<END
+			print $fh <<END
 	<feature name="Line spacing" value="Normal" tag="$line_gap_tag">
-		<!-- edit the below lines to provide the correct line metrics -->
 		<!-- Doulos -->
-		<value name="Normal" tag="n">
+		<value name="Normal" tag="$normal_tag">
 			<cmd name="null" args="2324 810"/>
 		</value>
-		<value name="Tight" tag="t">
+		<value name="Tight" tag="$tight_tag">
 			<cmd name="line_metrics" args="1420 442 307 1825 443 1825 443 87"/>
 		</value>
-		<value name="Loose" tag="l">
+		<value name="Loose" tag="$loose_tag">
 			<cmd name="line_gap" args="2800 1100"/>
 		</value>
 	</feature>
@@ -768,11 +735,11 @@ sub Interactions_output($\%\%\%\%)
 	print $feat_all_fh "\t<interactions>\n";
 
 	my $featset;
-    foreach $featset (sort sort_tests keys %$featset_to_usvs)
-    {
-    	my @featsets = split(/\s/, $featset);
-    	if (scalar @featsets == 1) {next;} #handled with <feature> elements
-    	
+	foreach $featset (sort sort_tests keys %$featset_to_usvs)
+	{
+		my @featsets = split(/\s/, $featset);
+		if (scalar @featsets == 1) {next;} #handled with <feature> elements
+		
 		#start test element
 		print $fh "\t\t<test select=\"$featset\">\n";
 		
@@ -793,12 +760,14 @@ sub Interactions_output($\%\%\%\%)
 		#I think that VIt and ROt should be handled the same way
 		#Using feat_del & feat_add for both VIt and ROt has problems 
 		# if both VIt and ROt are on since the second one processed can't delete ccmp_latin
+		# because the first one will have already deleted it
 		#Using a test select="VIt ROt" to solve this problem would create two features with 
 		# the same OT name (ccmp) and redundant lookups
 		#Using the lookup_add approach doesn't require testing for both VIt and ROt
 		# and avoids the above problem
 		
-		if ($featset =~ /VI-t/ and not $opt_g)
+		my $viet_featset = "$feats->{'1029'}{'tag'}-$feats->{'1029'}{'settings'}{'1'}{'tag'}";
+		if ($featset =~ /$viet_featset/ and not $opt_g)
 		{#hard-coded
 			print $fh "\t\t\t<cmd name=\"lookup_add\" args=\"GSUB {ccmp_latin} {viet_decomp}\"/>\n";
 			print $fh "\t\t\t<cmd name=\"lookup_add\" args=\"GSUB {ccmp_latin} {viet_precomp}\"/>\n";
@@ -807,7 +776,8 @@ sub Interactions_output($\%\%\%\%)
 			#print $fh "\t\t\t<cmd name=\"feat_del\" args=\"GSUB latn {IPA} {ccmp_latin}\"/>\n";
 			#print $fh "\t\t\t<cmd name=\"feat_add\" args=\"GSUB latn {IPA} {ccmp_vietnamese} 0\"/>\n";
 		}
-		if ($featset =~ /RO-t/ and not $opt_g)
+		my $rmnian_featset = "$feats->{'1041'}{'tag'}-$feats->{'1041'}{'settings'}{'1'}{'tag'}";
+		if ($featset =~ /$rmnian_featset/ and not $opt_g)
 		{#hard-coded
 			print $fh "\t\t\t<cmd name=\"lookup_add\" args=\"GSUB {ccmp_latin} {rom_decomp}\"/>\n";
 			print $fh "\t\t\t<cmd name=\"lookup_add\" args=\"GSUB {ccmp_latin} {rom_precomp}\"/>\n";
@@ -815,12 +785,12 @@ sub Interactions_output($\%\%\%\%)
 		
 		#encode cmds for all affected usvs
 		my %used_usvs;
-    	Test_output($feat_all_fh, $featset, %used_usvs, 
-    				%$featset_to_usvs, %$usv_feat_to_ps_name, %$dblenc_usv) unless $opt_g;
+		Test_output($feat_all_fh, $featset, %used_usvs, 
+					%$featset_to_usvs, %$usv_feat_to_ps_name, %$dblenc_usv) unless $opt_g;
 		
 		#end test element
 		print $fh "\t\t</test>\n";
-    }
+	}
 
 	#end interactions element
 	print $feat_all_fh "\t</interactions>\n";
@@ -875,7 +845,7 @@ my ($font_fn, $gsi_fn, $dblenc_fn, $feat_all_fn);
 
 getopts($opt_str); #sets $opt?'s & removes the switch from @ARGV
 
-#build a file containing a hash of feature & setting info
+#build a file containing a hash of feature & setting names to tags
 # to paste into this program for specifying tags
 if ($opt_l)
 {
@@ -892,20 +862,30 @@ if ($opt_l)
 	{
 		my $feat_t = $feats{$feat_id};
 		my ($tag, $name, $default) = ($feat_t->{'tag'}, $feat_t->{'name'}, 
-		                              $feat_t->{'default'});
-		print FILE "\t'$name' => '$tag',\n";
+									  $feat_t->{'default'});
+		if (not defined $tags{$name})
+			{print FILE "\t'$name' => '$tag',\n"; $tags{$name} = $tag;}
 		foreach my $set_id (@{$feats{$feat_id}{'settings'}{' ids'}})
 		{
 			my $set_t = $feat_t->{'settings'}{$set_id};
 			($tag, $name) = ($set_t->{'tag'}, $set_t->{'name'});
-			print FILE "\t'$name' => '$tag',\n";
+			if (not defined $tags{$name})
+				{print FILE "\t'$name' => '$tag',\n"; $tags{$name} = $tag;}
 		}
 	}
 	
-	
 	#my $line_gap_tag = Tag_get('Line spacing', 2);
-	my $line_gap_tag = Tag_lookup('Line spacing', %nm_to_tag);
-	print FILE "\t'Line spacing' => '$line_gap_tag',\n";
+	#print FILE "\t'Line spacing' => '$line_gap_tag',\n";
+	
+	foreach my $s ('Line spacing', 'Tight', 'Normal', 'Loose')
+	{
+		if (not defined $tags{$s})
+		{
+			my $tag = Tag_lookup($s, %nm_to_tag);
+			print FILE "\t'$s' => '$tag',\n";
+			$tags{$s} = $tag;
+		}
+	}
 	
 	print FILE ");\n";
 	exit;	
