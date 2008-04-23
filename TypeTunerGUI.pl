99503cb0d900ac6e022c58e1dbc73d34581f166e
Alan Ward <alan_ward@sil.org>
2008-04-23 21:35:23 +0000

    Archive and undo work on adding a GTK+ GUI. Rename TypeTuner.pl to TypeTunerGUI.pl. This provides a wrapper around TypeTuner.pm which is also being renamed. Rename TypeTuner.pm to TypeTuner.pl.
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@127 4150712b-26e8-44b9-9db8-fb9803e0bbe7

---
+++ TypeTunerGUI.pl
@@ -0,0 +1,34 @@
+# © SIL International 2007. All rights reserved.
+# Please do not redistribute.
+
+use TypeTuner;
+
+if (scalar @ARGV != 0)
+	{TypeTuner::cmd_line_exec(@ARGV); exit();}
+
+#start the GUI if there are no command line arguments
+use Gtk2 '-init';
+use Gtk2::GladeXML;
+
+$gladefn = 'typetuner.glade';
+$gladexml = Gtk2::GladeXML->new($gladefn) || die "Can't open $gladefn";
+$gladexml->signal_autoconnect_from_package('main');
+$closebtn = $gladexml->get_widget('Close'); 
+Gtk2->main();
+
+sub on_main_delete_event
+{
+	#return 0;
+	Gtk2->main_quit();
+}
+
+#This sub isn't connected, but it was mentioned in the Glade doc
+sub gtk_main_quit
+{
+	Gtk2->main_quit();
+}
+
+sub on_b_Close_clicked
+{
+	TypeTuner::cmd_line_exec('-d', 'applyset_xml', 'feat_all.xml', 'feat_set.xml', 'zork.ttf');
+}
