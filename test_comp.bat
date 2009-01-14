9078bf107b1a8d75f19bf331c29799d2806069b1
Alan Ward <alan_ward@sil.org>
2009-01-14 20:38:48 +0000

    Delete files related to testing RFComposer. Use real RFS files for testing now.
    git-svn-id: file:///home/mhosken/Work/MSEA/Personal/TimEves/subversion/repositories/typetuner/trunk@144 4150712b-26e8-44b9-9db8-fb9803e0bbe7

--- test_comp.bat
+++
@@ -1,5 +0,0 @@
-rem gsi_zork_test.xml indicates three features interact on one glyph
-rem  (though the glyph really does NOT do this)
-rem it also contains corrections to the GSI
-cls
-perl rfcomposer.pl -d zork.ttf gsi_zork_test.xml dblenc_eggs.txt
